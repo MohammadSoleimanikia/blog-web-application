91b8c40746fb3b6b05b94f37ceeade393fa5cb65
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-10-05 22:17:36 +0330

    add new posts ejs and add delete button (without functionality)

--- index.js
+++ index.js
@@ -7,6 +7,18 @@ app.use(express.static('public'));
 app.get('/',(req,res)=>{
     res.render('index.ejs')
 })
+app.get('/post1',(req,res)=>{
+    res.render('post1.ejs')
+})
+app.get('/post2',(req,res)=>{
+    res.render('post2.ejs')
+})
+app.get('/post3',(req,res)=>{
+    res.render('post3.ejs')
+})
+app.get('/post4',(req,res)=>{
+    res.render('post4.ejs')
+})
 app.listen(port,()=>{
     console.log(`app started at port ${port}`)
 })
