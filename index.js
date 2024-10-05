07598fb8e35fd6e3af9d1951095d84831fbcb958
MohammadSoleimanikia <121093560+MohammadSoleimanikia@users.noreply.github.com>
2024-10-05 18:44:14 +0330

    add first styling and header and posts

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ import ejs from "ejs"
 
 const port=3000;
 const app=express();
+app.use(express.static('public'));
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
 app.listen(port,()=>{
     console.log(`app started at port ${port}`)
 })
